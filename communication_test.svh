f7ceb42622b6068f7949d421f94d2e8f509bdbce
SteveYaoLin <yaolin0616@163.com>
2024-06-10 19:20:27 +0800

    UVM 用 uvm_put_port, uvm_get_port 和 uvm_tlm_fifo 提供了线程间通信机制。我们看到了, 任何 想要跟另外的线程通信的对象需要例化一个端口, 而这个端口需要连接到 FIFO

--- communication_test.svh
+++ communication_test.svh
@@ -20,19 +20,20 @@ class communication_test extends uvm_test;
    consumer consumer_h;
    uvm_tlm_fifo #(int) fifo_h;
 
-   function new(string name, uvm_component parent);
-      super.new(name, parent);
-   endfunction : new
-
    function void build_phase(uvm_phase phase);
       producer_h = new("producer_h", this);
       consumer_h = new("consumer_h", this);
       fifo_h = new("fifo_h",this);
    endfunction : build_phase
 
+   function new(string name, uvm_component parent);
+      super.new(name, parent);
+   endfunction : new
+
    function void connect_phase(uvm_phase phase);
       producer_h.put_port_h.connect(fifo_h.put_export);
       consumer_h.get_port_h.connect(fifo_h.get_export);
-   endfunction : connect_phase
 
+   endfunction : connect_phase
 endclass : communication_test
+
